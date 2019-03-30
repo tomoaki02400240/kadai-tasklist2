ba91a7760bec7ca94633aacb03b6d232431aaf3a
tomo.iwamoto <akirouakirouakirou@yahoo.co.jp>
2019-03-30 16:35:49 +0000

    edit made

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+   def show
+       @task = Task.find(params[:id])
+   end
+   def new 
+       @task = Task.new
+   end
+   def create
+       @task = Task.new(task_params)
+       if @task.save
+           flash[:success] = 'タスク完了'
+           redirect_to @task
+       else 
+           flash.now[:danger] = 'タスク失敗'
+         render :new
+       end
+   end
+   def edit
+       @task = Task.find(params[:id])
+   end
+   def update
+       @task = Task.find(params[:id])
+       if @task.update(task_params)
+           flash[:success] = '編集しました'
+           redirect_to @task
+       else
+           flash.now[:danger] = '編集に失敗しました'
+           render :edit
+       end
+   end
+   def destroy
+   end
+   
+   private
+   def task_params
+       params.require(:task).permit(:content)
+   end
+end
