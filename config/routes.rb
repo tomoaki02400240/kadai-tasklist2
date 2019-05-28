fa90b212517e18819ac6fb27e563da58ca18688b
tomo.iwamoto <akirouakirouakirou@yahoo.co.jp>
2019-05-28 13:40:30 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
  root to: 'tasks#index'
  resources :tasks
+ 
+ get 'signin', to: 'sessions#new'
+ post 'signin', to: 'sessions#create'
+ delete 'signout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190403135926) do
+ActiveRecord::Schema.define(version: 20190528130926) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
